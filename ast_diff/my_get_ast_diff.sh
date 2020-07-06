6f87d6a7529e84a4f70bb87a24510433a75ba373
Tao Zhu <zhutaosx@gmail.com>
2020-07-06 13:42:18 -0700

    Replace /Users/zhutao/lab/gh-crawler for hardcoded path name

--- ast_diff/my_get_ast_diff.sh
+++ ast_diff/my_get_ast_diff.sh
@@ -1,2 +1,3 @@
-export CRAWLER_HOME=/Users/zhutao/lab/gh-crawler
-python3 ast_diff/get_ast_diff.py --mode standalone --input_folder ../data/one-diff-dataset --output_folder ../data/ast-diff
+export CRAWLER_HOME=$(pwd)
+#echo $CRAWLER_HOME
+python3 ast_diff/get_ast_diff.py --mode standalone --input_folder ../data/one-diff-dataset --output_folder ../data/ast-json-dataset
