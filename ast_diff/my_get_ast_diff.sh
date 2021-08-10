ad547b1f7d39baa92f087317ccabf407b25f2a7b
nashid <noor.nashid1@gmail.com>
2021-08-10 12:33:29 -0700

    changes in the ast diff script

--- ast_diff/my_get_ast_diff.sh
+++ ast_diff/my_get_ast_diff.sh
@@ -1,10 +1,11 @@
-export BABEL_DIR=$(pwd)/../data/babel-dir/
-export NODE_PATH=$(pwd)/../data/node_modules/
+export BABEL_DIR=$(pwd)/babel-dir/
+export NODE_PATH=$(pwd)/node_modules/
 
 export CRAWLER_HOME="$(pwd)"
-source=../data/small_raw
-target=../data/small_astJSON
+source=/home/contextml/context-ml/gnn-create-dataset/gnn-dataset/bin-operator/single-statement/data
+target=/home/contextml/hoppity-data/ml_astJSON
 echo $source
 echo $target
 #echo $CRAWLER_HOME
-python ast_diff/get_ast_diff.py --mode standalone --input_folder $source --output_folder $target
+python ast_diff/get_ast_diff.py --mode standalone --input_folder $source --output_folder $target --np 1
+
